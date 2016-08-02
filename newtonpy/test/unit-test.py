284c3be43d5b6604bbb1f687bb6172a97f5e96cb
Christoph Huck <Christoph@Yoda.fritz.box>
2016-08-02 14:54:07 +0200

    added newton method and unit-test

---
+++ newtonpy/test/unit-test.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class NewtonTest(unittest.TestCase):
+    def test_sin(self):
+        print("test_sin")
+        # TEST CODE
+
+    def test_polynomial(self):
+        print("test_polynomial")
+        # TEST CODE
