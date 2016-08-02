4eccd03e08143fe0535aa2297f0c81b62804d719
Christoph Huck <Christoph@Yoda.fritz.box>
2016-08-02 15:10:51 +0200

    added newton and travis file

---
+++ newtonpy/test/newton.py
@@ -0,0 +1,23 @@
+from __future__ import division
+
+from numpy import abs, cos, sin
+
+
+def newton(fun, gradient, init_value):
+    tol = 1e-8
+
+    x = init_value
+    f = fun(x)
+    df = gradient(x)
+    error = abs(fun(x))
+    iter = 0
+    while error > tol and iter < 100:
+        iter += 1
+        dx = f/df
+        x = x-dx
+        f = fun(x)
+        df = gradient(x)
+        error = abs(f)
+    if iter == 100:
+        print("max iter reached")
+    return x
